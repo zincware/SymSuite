bb743ebb7cd065ad0a5fe812990ce902142c8646
SamTov <tovey.samuel@gmail.com>
2021-03-11 10:10:01 +0100

    Update generator calculation

--- symdet/generators/extract_generators.py
+++ symdet/generators/extract_generators.py
@@ -43,24 +43,22 @@ class GeneratorExtract:
         self.point_pairs = []
         self.generators = []
 
-    def _perform_PCA(self, data):
+    @staticmethod
+    def _perform_PCA(data):
         """
         Perform PCA on the data to remove redundant data
         """
 
-        pca = PCA(n_components=4)
+        pca = PCA(n_components=1)
         pca.fit(data)
 
         print(pca.components_)
-        print(pca.explained_variance_)
-        print(pca.n_components_)
-        print(pca.n_features_)
+        print(pca.explained_variance_ratio_)
 
-
-    def _gram_schmidt_process(self):
+    def _gram_schmidt_process(self, vectors):
         """ Implement the gram schmidt process on data """
 
-        for vector in self.basis_data:
+        for vector in vectors:
             u = vector
 
             for basis_vector in self.basis:
@@ -72,13 +70,11 @@ class GeneratorExtract:
             else:
                 self.basis.append(u)
 
-        self.basis = self.basis[:-1]
-
     def _normalize_basis_set(self):
         """ Normalize the generated basis set"""
-        
+
         for i in range(len(self.basis)):
-            self.basis[i] = self.basis[i]/np.linalg.norm(self.basis[i])
+            self.basis[i] = self.basis[i] / np.linalg.norm(self.basis[i])
 
     @staticmethod
     def _projection_operator(u, v):
@@ -98,7 +94,7 @@ class GeneratorExtract:
                             The v vector for the projection operation
         """
 
-        return (np.dot(u, v)/np.dot(u, u))*u
+        return u*(np.dot(u, v) / np.dot(u, u))
 
     def _start_gs_method(self):
         """ Populate basis set with first two vectors
@@ -106,16 +102,38 @@ class GeneratorExtract:
         Randomly sample the given data and construct the first two vectors for the orthonormal basis set
         """
 
-        index = random.randint(0, len(self.basis_data)-1)  # generate a random index
+        index = random.randint(0, len(self.basis_data) - 1)  # generate a random index
         u_1 = self.basis_data[index]  # get the first basis vector
         self.basis.append(u_1)  # assign first vector to basis
         self.basis_data.pop(index)  # remove the data point from the list
 
-        index = random.randint(0, len(self.basis_data)-1)  # generate a new random index
+        index = random.randint(0, len(self.basis_data) - 1)  # generate a new random index
         v_2 = self.data_clusters[index]  # select data at that index
         u_2 = v_2 - self._projection_operator(u_1, v_2)  # calculate the orthogonal vector
         self.basis.append(u_2)  # add vector to the basis set
         self.basis_data.pop(index)  # remove the data point from the data
+
+    def _construct_standard_basis(self):
+        """
+        Construct a standard basis dependant on the size of the point cloud.
+        """
+        zero_vector = np.zeros((len(self.data_clusters), len(self.data_clusters)))  # build the zero matrix
+
+        for i, vector in enumerate(zero_vector):
+            vector[i] = 1
+
+        return zero_vector
+
+    def _eliminate_closest_vectors(self, data: np.array):
+        """
+        Find and remove the vectors in data closest to the basis vectors of the class.
+        """
+
+        size_array = []  # instantiate empty array for the sizes of the data
+
+        for vector in data:
+            distance = np.linalg.norm
+
 
     def _build_orthonormal_basis(self):
         """ Construct and orthonormal basis
@@ -126,18 +144,22 @@ class GeneratorExtract:
         """
 
         self._start_gs_method()  # get the first two vectors in the basis set
-        self._gram_schmidt_process()  # perform the Gram-Schmidt process over the rest of the array
+        tst_data = [[1, 0], [0, 1]]
+        self._gram_schmidt_process(tst_data)  # perform the Gram-Schmidt process over the rest of the array
         self._normalize_basis_set()  # normalize the vectors in the orthogonal basis
 
     def _find_hyperplane_points(self):
         """ Get all points within the hyperplane thickness """
 
-        for point in self.data_clusters:
-            for basis_vector in self.basis[2:]:
-                if abs(np.dot(point, basis_vector)) < self.delta:
-                    self.hyperplane_points.append(point)
-                else:
-                    continue
+        if len(self.basis) == 2:
+            self.hyperplane_points = self.data_clusters
+        else:
+            for point in self.data_clusters:
+                for basis_vector in self.basis[2:]:
+                    if abs(np.dot(point, basis_vector)) < self.delta:
+                        self.hyperplane_points.append(point)
+                    else:
+                        continue
 
     def _find_point_pairs(self):
         """ Find all pairs connected by a generator """
@@ -154,8 +176,8 @@ class GeneratorExtract:
     def _sigma_function(self, points):
         """ Get the directional information from points on hyperplane """
 
-        return np.sign(((np.dot(points[0], self.basis[0]))*(np.dot(points[1], self.basis[1]))) -
-                       ((np.dot(points[0], self.basis[1]))*(np.dot(points[1], self.basis[0]))))
+        return np.sign(((np.dot(points[0], self.basis[0])) * (np.dot(points[1], self.basis[1]))) -
+                       ((np.dot(points[0], self.basis[1])) * (np.dot(points[1], self.basis[0]))))
 
     def _solve_for_generator(self, pair, reference):
         """ Perform regression task on generator equation
@@ -176,7 +198,7 @@ class GeneratorExtract:
         normalization = np.linalg.norm(point_1)
         sigma = self._sigma_function(pair)
 
-        lhs = (normalization*distance)/(sigma*distance_magnitude)
+        lhs = (normalization * distance) / (sigma * distance_magnitude)
 
         regression_tensor_1 = [lhs[0], 0]
         regression_tensor_2 = [lhs[1], 0]
@@ -194,9 +216,9 @@ class GeneratorExtract:
         for points in self.point_pairs:
             for basis in self.basis[2:]:
                 generator = self._solve_for_generator(points, basis)
-                check = generator*basis
+                check = generator * basis
                 for basis_check in self.basis[2:]:
-                    check += generator*basis_check
+                    check += generator * basis_check
                 condition = []
                 for row in check:
                     if all(v == 0 for v in row):
@@ -232,13 +254,13 @@ class GeneratorExtract:
         """
 
         counter = 0
-        while counter < 100:
-            #self._perform_PCA()              # 1.) Perform pca on the input data
+        while counter < 10:
+            # self._perform_PCA()              # 1.) Perform pca on the input data
             self._build_orthonormal_basis()  # 2.) Build the orthonormal basis
-            self._find_hyperplane_points()   # 3.) Fill the hyperplane points array
-            self._find_point_pairs()         # 4.) Find all connected points
-            self._evaluate_generator()       # 5.) Evaluate generators
-            self._clear_class_state()        # Clear class state for next run
+            self._find_hyperplane_points()  # 3.) Fill the hyperplane points array
+            self._find_point_pairs()  # 4.) Find all connected points
+            self._evaluate_generator()  # 5.) Evaluate generators
+            self._clear_class_state()  # Clear class state for next run
             counter += 1
 
         outcome = []
