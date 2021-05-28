5a627cde7d78b5a045dd6f28c6928d4187e7d267
SamTov <tovey.samuel@gmail.com>
2021-05-28 08:22:10 +0200

    Update CI structure

--- CI/integration_tests/test_generator_extraction.py
+++ CI/integration_tests/test_generator_extraction.py
@@ -7,10 +7,24 @@ from symdet.generators.generators import GeneratorExtraction
 import numpy as np
 
 
-class SO2Test(unittest.TestCase):
+class TestSO2(unittest.TestCase):
     """
     Test SO(2) generator extraction works.
     """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Prepare the test data.
+
+        Returns
+        -------
+
+        """
+        cls.circle = SO2(n_points=500,
+                         noise=True,
+                         variance=0.05)
+        cls.circle.generate_data()
 
     def test_circle_generation(self):
         """
@@ -20,10 +34,7 @@ class SO2Test(unittest.TestCase):
         -------
         Asserts that the mean of the coordinate norms is approx 1.
         """
-        self.circle = SO2(n_points=500,
-                          noise=True,
-                          variance=0.05)
-        self.circle.generate_data()
+
         self.assertAlmostEqual(float(np.mean(np.linalg.norm(self.circle.data, axis=1))), 1, delta=0.01)
 
     def test_generator_extraction(self):
@@ -34,10 +45,6 @@ class SO2Test(unittest.TestCase):
         -------
         Asserts the correct generator is found.
         """
-        self.circle = SO2(n_points=500,
-                          noise=True,
-                          variance=0.05)
-        self.circle.generate_data()
         generator_extractor = GeneratorExtraction(self.circle.data,  # clustered data
                                                   delta=0.5,  # distance of points to hyperplane
                                                   epsilon=0.3,  # distance between points connected by a generator
@@ -46,7 +53,7 @@ class SO2Test(unittest.TestCase):
                                                                                                plot=False)
         self.assertAlmostEqual(self.variance_list[0], 1, delta=0.0001)
         self.assertAlmostEqual(self.variance_list[1], 0, delta=0.00000001)
-        generator = np.sqrt(self.generators[0]**2)
+        generator = np.sqrt(self.generators[0] ** 2)
         self.assertAlmostEqual(generator[0], 0.0, delta=0.05)
         self.assertAlmostEqual(generator[1], 1.0, delta=0.1)
         self.assertAlmostEqual(generator[2], 1.0, delta=0.1)
