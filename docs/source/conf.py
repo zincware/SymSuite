cdcc4125c294636eac89691bacdf7167b673b950
SamTov <tovey.samuel@gmail.com>
2021-07-08 18:35:03 +0200

    update theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,12 @@ Configuration file for the Sphinx documentation builder.
 """
 import os
 import sys
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../../"))
-
-html_theme = "sphinx_material"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
