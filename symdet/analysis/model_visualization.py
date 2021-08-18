a5e2f7873864bf89e1102b9f2a6ea5294d05582a
SamTov <tovey.samuel@gmail.com>
2021-08-18 09:05:01 +0200

    Fix license files

--- symdet/analysis/model_visualization.py
+++ symdet/analysis/model_visualization.py
@@ -1,18 +1,10 @@
 """
-This file is part of the SymDet distribution (https://github.com/SamTov/SymDet).
-Copyright (c) 2021 Samuel Tovey.
+This program and the accompanying materials are made available under the terms of the
+Eclipse Public License v2.0 which accompanies this distribution, and is available at
+https://www.eclipse.org/legal/epl-v20.html
+SPDX-License-Identifier: EPL-2.0
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+Copyright Contributors to the Zincware Project.
 
 Model Visualization
 ===================
